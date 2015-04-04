0256c0a800a30aadd478c2eb924d113503b7935f
sicks <tcopeland@dg3n.net>
2015-04-04 06:51:51 +0000

    new step definition

---
+++ features/step_definitions/user_steps.rb
@@ -0,0 +1,34 @@
+# Helpers
+# =======
+def google_visitor
+  @visitor ||= { provider: "google", uid: "123456789" }
+end
+
+def delete_user
+  @user ||= User.includes(:auths).find_by("auth.uid" => @visitor[:uid])
+  @user.destroy unless @user.empty?
+end
+
+# Givens
+# ======
+Given(/^I do not exist as a user$/) do
+  google_visitor
+  delete_user
+end
+
+# Whens
+# =====
+When(/^I sign in via google auth$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+# Thens
+# =====
+Then(/^a new user should be created$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should be signed in$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
