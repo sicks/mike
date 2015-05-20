015de61c37519d0e958582b713bcbe8f4bb527fe
sicks <tcopeland@dg3n.net>
2015-05-20 06:14:47 +0000

    vcr config fix?

--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -5,7 +5,7 @@ end
 
 RSpec.configure do |c|
   c.around(:example, :vcr) do |example|
-    options = { erb: true, record: :once }
+    options = { erb: true, record: :once, allow_playback_repeats: true }
     VCR.use_cassette(:eaal, options) { example.call }
   end
 end
