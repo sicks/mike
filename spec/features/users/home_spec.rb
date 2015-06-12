94876f6610206dbe8fd48ca423bc99acc25e51e3
sicks <tcopeland@dg3n.net>
2015-06-12 16:38:40 +0000

    there is no homepage

--- spec/features/users/home_spec.rb
+++ spec/features/users/home_spec.rb
@@ -1,21 +1,2 @@
 require 'rails_helper'
 
-RSpec.feature 'User index page', type: :feature do
-  let!(:user) { create(:user) }
-  before(:example) do
-    login_with_omniauth
-  end
-
-  scenario 'User has no api keys', :vcr do
-    user.api_keys.destroy_all
-    click_link 'Home'
-    expect(page).to have_content "an api key is required"
-  end
-
-  scenario 'User has api keys', :vcr do
-    user.api_keys.create!( attributes_for(:api_key).except(:user_id) )
-    click_link 'Home'
-    expect(page).to have_content "DKP Index"
-  end
-
-end
